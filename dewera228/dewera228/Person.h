f4ea5d1a06b13bbbf9fe600c7a64b2fab64be778
dewera228 <dewera228@gmail.com>
2024-10-31 20:21:54 +0300

    finish labirint

--- dewera228/dewera228/Person.h
+++ dewera228/dewera228/Person.h
@@ -1,16 +1,23 @@
-#pragma once
+#ifndef PLAYER_H
+#define PLAYER_H
+
 #include <SDL.h>
-#include "Maze.h"  
+#include "Maze.h"
+
+const int SPEED = 4;
 
 class Player {
 public:
-    void Person(int startX, int startY, Maze* m);
-    void move(int dx, int dy);
-    bool hasReachedExit();
+    Player(int x, int y, Maze* maze, SDL_Renderer* renderer);
+    void handleInput(const Uint8* state);
     void draw(SDL_Renderer* renderer);
-
-    SDL_Rect rect;
+    bool hasReachedExit();
 
 private:
+    SDL_Rect rect;
+    SDL_Texture* texture;
     Maze* maze;
+    SDL_Renderer* renderer;
 };
+
+#endif // PLAYER_H
--- dewera228/dewera228/dewera228.cpp
+++ dewera228/dewera228/dewera228.cpp
@@ -1,5 +1,4 @@
-﻿// main.cpp
-#include "Game.h"
+﻿#include "Game.h"
 
 int main(int argc, char* argv[]) {
     Game game;
